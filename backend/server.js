7d7cfe8deac9cba07807c33cff3662b8d533add9
BigBWolf <vito.ilchev@gmail.com>
2020-05-12 18:42:21 +0200

    Fixing loading pages after adding a question or posting an answer to the correct page and post.

--- backend/server.js
+++ backend/server.js
@@ -29,10 +29,11 @@ app.use(bodyParser.urlencoded({
 app.use(cors());
 app.use('/questions', questionRoute);
 
-if(process.env.NODE_ENV === 'production') {
+//Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html') );
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html') );
     })
 }
 
